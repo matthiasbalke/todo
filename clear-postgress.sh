764430f2b91fe99be4d35458a4c43db65112370a
Matthias Balke <Matthias.Balke@googlemail.com>
2026-03-30 03:33:11 +0200

    helper script to reinitialize testing db

--- clear-postgress.sh
+++ clear-postgress.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+echo "============================================"
+echo "  Do you want to drop all tables and data?"
+echo "============================================"
+echo ""
+
 docker compose stop postgres
 docker compose rm -f postgres
 docker compose up -d postgres
