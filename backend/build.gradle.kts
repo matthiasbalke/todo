fabb5995aa0b4eeda6444d95f29a04830dc4fadb
Matthias Balke <matthias.balke@googlemail.com>
2026-06-10 11:09:41 +0200

    Upgrade Spring Gradle plugins to 2.4.0
    implement:
    * #100 
    * #101 
    * #102

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-	kotlin("jvm") version "2.3.21"
-	kotlin("plugin.spring") version "2.3.21"
+	kotlin("jvm") version "2.4.0"
+	kotlin("plugin.spring") version "2.4.0"
 	id("org.springframework.boot") version "4.0.6"
 	id("io.spring.dependency-management") version "1.1.7"
-	kotlin("plugin.jpa") version "2.3.21"
+	kotlin("plugin.jpa") version "2.4.0"
 }
 
 group = "com.github.matthiasbalke"
